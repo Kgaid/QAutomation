e43c5948802eb48efa9d95d98730ef2a0f95cfa4
¨K_Gaid¨ <kateryna.gaid@gmail.com>
2023-05-25 23:34:01 +0200

    up to Lesson 11

---
+++ Lesson11/Homework11/Task111.py
@@ -0,0 +1,57 @@
+class Human:
+    default_name = 'Unknown'
+    default_age: int = int(30)
+
+    def __init__(self, name: str = default_name, age: int = default_age):
+        self.name = name
+        self.age = age
+        self.__money = 0
+        self.__house = None
+
+    @staticmethod
+    def default_info():
+        return Human.default_name, Human.default_age
+
+    def __make_deal(self, house, price):
+        if self.__money >= price:
+            self.__money -= price
+            self.__house: Human = house
+            print("You have bought a house!")
+        else:
+            print("You have no enough money for this. You need to earn")
+
+    def earn_money(self, accumulation):
+        self.__money += accumulation
+
+    def buy_house(self, house, discount):
+        last_price = house.final_price(discount)
+        self.__make_deal(house, last_price)
+
+    def info(self):
+        print(f"The name is {self.name}")
+        print(f"The age is {self.age}")
+        print(f"Amount of money: {self.__money}")
+        print(f"House: {self.__house}")
+
+
+class House:
+
+    def __init__(self, area: int, price: int):
+        self._area = area
+        self._price = price
+
+    def final_price(self, discount: int):
+        return self._price - discount
+
+
+class SmallHouse(House):
+    def __init__(self):
+        super().__init__(area=40, price=100000)
+
+
+alice = Human(name="Alice", age=38)
+alice.info()
+appartment = SmallHouse()
+alice.buy_house(appartment, discount=10000)
+alice.earn_money(99000)
+alice.buy_house(appartment, discount=5000)
--- Lesson7/Homework7/Task71.py
+++ Lesson7/Homework7/Task71.py
@@ -1,1 +1,4 @@
-with open('test')
+with open('test_file.csv', "r+") as file1:
+    list_of_file = file1.readlines()
+    new_list =value.split(',')
+    print(new_list)
